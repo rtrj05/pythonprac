a395bcd338b06240cdd93a64741930ec97680664
rtrj05 <rtrj2004jha@gmail.com>
2025-12-27 12:47:10 +0530

    added dictionary file

---
+++ dict.py
@@ -0,0 +1,15 @@
+marks ={
+   "Rituraj": 95,
+    "Amit":92,
+   "ghoului":85 
+   }
+ 
+print(marks["Amit"])
+
+arr = [1,2,2,3,3,3]
+freq = {}
+
+for x in arr:
+    freq[x] = freq.get(x, 0) + 1
+
+print(freq)
